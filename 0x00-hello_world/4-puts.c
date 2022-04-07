a7e0304dde20b47f7d89dbf56433b2e6919b3860
FrankDonny <arkohfrank1@gmail.com>
2022-04-07 21:21:35 +0000

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 after main
+ */
+int main(void)
+{
+	int putchar("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
