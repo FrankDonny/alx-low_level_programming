e1a97b249dd2ad7374959eaa286fd7b668d43327
FrankDonny <arkohfrank1@gmail.com>
2022-04-08 16:44:29 +0000

    print lower char

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+		putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ * Return: 0 always
+ */
+int main(void)
+
+	char i;
+	char c;
+
+	i = 65;
+	c = 97
+	if (i < 91)
+	{
+		putchar(i);
+	}
+	else
+		if (c < 123)
+			putchar(c);
+		else break;
+	return (0);
+}
