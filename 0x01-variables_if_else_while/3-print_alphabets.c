f36e74308b93215ff20ece9e96da355093eb686e
FrankDonny <arkohfrank1@gmail.com>
2022-04-08 17:58:52 +0000

    print alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ int main(void)
 	char a[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i <= 52; i++)
+	for (i = 0; i < 52; i++)
 		putchar(a[i]);
 	putchar('\n');
 	return (0);
