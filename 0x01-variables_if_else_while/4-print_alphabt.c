03bbba9e5f23fee3e62809a82d281c1e0fe0f861
FrankDonny <arkohfrank1@gmail.com>
2022-04-08 18:34:25 +0000

    print alph w/o e,o

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char a[26] = "abcdfghijklmnoprstuvwxyz";
+	char a[24] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 24; i++)
 		putchar(a[i]);
 	putchar('\n');
 	return (0);
