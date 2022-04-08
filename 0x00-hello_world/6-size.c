860b7c6a0026d5b41a1434af51aa3d95d279c7b1
FrankDonny <arkohfrank1@gmail.com>
2022-04-08 01:00:15 +0000

    display datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,9 @@
 
 int main(void)
 {
-	int a;
-	char b;
-	float c;
-	double d;
-
-	printf("Size of a char: %lu\n", (unsigned long)sizeof(b));
-	printf("Size of an int: %d\n", (signed int)sizeof(a));
-	printf("Size of a long int: %ld\n", (long int)sizeof(b));
-	printf("Size of a float: %f\n", (float)sizeof(c));
-	printf("Size of a double: %lf\n", (double)sizeof(d));
+	printf("Size of a char: %d\n", sizeof(char));
+	printf("Size of an int: %d\n", sizeof(int));
+	printf("Size of a long int: %d\n", sizeof(long int));
+	printf("Size of a float: %d\n", sizeof(float));
 	return (0);
 }
