e7facbc66162403c226aa15b644038692796de3a
FrankDonny <arkohfrank1@gmail.com>
2022-04-09 00:47:46 +0000

    comb numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,9 @@ int main(void)
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-		if (i != 57);
-		{
-			putchar(' ');
-			putchar(',');
-		}
+		if (i != 57)
+		putchar(' ');
+		putchar(',');
 	}
 	putchar('\n');
 	return (0);
