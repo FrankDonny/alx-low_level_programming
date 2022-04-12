0f9c096fca540f322dbfc3ff96be61095e2648c1
FrankDonny <arkohfrank1@gmail.com>
2022-04-12 23:07:18 +0000

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,5 @@
-#include <unistd.h>
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-//int _putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
-
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
