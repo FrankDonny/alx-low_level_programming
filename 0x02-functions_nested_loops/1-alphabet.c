648db67d5c2477846843d5148e7c6a1c46984680
FrankDonny <arkohfrank1@gmail.com>
2022-04-12 16:46:49 +0000

    print alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0 always
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 97; a < 123; a++)
+		alpha(a);
+	alpha('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,7 @@
+#include <unistd.h>
+
 int _putchar(char c);
+char alpha(char c)
+{
+	return (write(1, &c, 1));
+}
