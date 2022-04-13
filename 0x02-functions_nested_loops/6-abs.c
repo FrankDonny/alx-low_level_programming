87c112a79671eec3e93a7fb0c8853fe499ffa2cb
FrankDonny <arkohfrank1@gmail.com>
2022-04-13 00:34:02 +0000

    absolute number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,14 +3,12 @@
 
 /**
  * _abs - computes the absolute value
- * @i - the number to compute
+ * @i: the number to compute
  * Return: absolute value
  */
 
 int _abs(int i)
 {
-	int i;
-
 	if (i < 0)
 	{
 		return (i * (-1));
