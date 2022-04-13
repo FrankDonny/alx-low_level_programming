1f84781e8e5716bfca43518b62590e9729afd3ee
FrankDonny <arkohfrank1@gmail.com>
2022-04-13 00:19:12 +0000

    print absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,12 @@
 #include "main.h"
 
 /**
- * _isalpha - indicates if letter is caps or lower case
- * @c: is the arg
- * Return: 1 if the letter is lower, else returns 0
+ * _abs - computes the absolute value
+ * int: the number to compute
+ * Return: absolute value
  */
 
 int _abs(int)
 {
-	int a = abs();
-	
 	return (abs(int));
 }
