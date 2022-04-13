307c07294b63949a49b93802bb12edec758229d4
FrankDonny <arkohfrank1@gmail.com>
2022-04-13 01:25:00 +0000

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - the function
+ * @a: an arg
+ * @b: an arg
+ * @c: an arg
+ * @d: an arg
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	while (d <= '9)
+	{
+		c = 0;
+		while (c < 6)
+			b = 0;
+			while (b < 5)
+				a = 0
+				while (a < 3)
+				{
+					_putchar('a');
+					_putchar('b');
+					_putchar(':');
+					_putchar('c');
+					_putchar('d');
+					a++;
+				}b++
+			}c++
+		}d++
+}
