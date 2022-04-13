090551633b1624384b53c5152ce03baaed259fa5
FrankDonny <arkohfrank1@gmail.com>
2022-04-13 00:45:25 +0000

    last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - indicates if letter is caps or lower case
+ * @i: is an arg
+ * @a: is an arg
+ * Return: last digit
+ */
+
+int print_last_digit(int i, int a)
+{
+	int last = i % a;
+
+	return (last);
+}
