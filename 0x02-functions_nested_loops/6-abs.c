0d14479fc6678c3de42a8af50f10b79cd16a3b9a
FrankDonny <arkohfrank1@gmail.com>
2022-04-13 00:14:02 +0000

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _isalpha - indicates if letter is caps or lower case
+ * @c: is the arg
+ * Return: 1 if the letter is lower, else returns 0
+ */
+
+int _abs(int)
+{
+	int a = abs();
+	
+	return (abs(int));
+}
