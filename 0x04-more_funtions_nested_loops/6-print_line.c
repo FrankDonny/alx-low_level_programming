a3dd3961f9cbc01fe829c32c25daacde65f14893
FrankDonny <arkohfrank1@gmail.com>
2022-04-15 00:39:38 +0000

    print line

--- 0x04-more_funtions_nested_loops/6-print_line.c
+++ 0x04-more_funtions_nested_loops/6-print_line.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * print_line - should print a sraight line
- * @n: an integer
- */
+* print_line - prints a line of _ n long
+* @n: length of line
+*
+* Return: void
+*/
+
 void print_line(int n)
 {
-	int line;
-	
-	for (line = 0; line < n; line++)
-	{
-		_putchar(95);
-		_putchar('\n');
-	}
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-}
+	int line = 0;
 
+	while (line < n)
+	{
+		_putchar('_');
+		line++;
+	}
+	_putchar('\n');
+}
