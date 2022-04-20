dc01c60e99d0c36dda620f1d2856d55cffa42b55
FrankDonny <arkohfrank1@gmail.com>
2022-04-20 18:17:22 +0000

    puts function

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,6 +2,11 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * _puts - prints strings
+ * str: arg
+ */
+
 void _puts(char *str)
 {
 	puts(str);
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
+#include <publib.h>
 
 void print_rev(char *s)
 {
