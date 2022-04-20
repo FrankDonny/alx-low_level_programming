1ee5164eae2980891516fbd3e46cf9c81d3f79f7
FrankDonny <arkohfrank1@gmail.com>
2022-04-20 21:21:43 +0000

    copy strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _strcpy - copies strings
+ * @dest: arg
+ * @src: arg
+ * Return: dest
+ */
+
 char *_strcpy(char *dest, char *src)
 {
 	strcpy(dest, src);
