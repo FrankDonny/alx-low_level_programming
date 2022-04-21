6bd81a0b4547f723cc030d3b2f728bd779ed5364
FrankDonny <arkohfrank1@gmail.com>
2022-04-21 00:11:20 +0000

    password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,13 +3,23 @@
 #include <time.h>
 
 /**
- * main - entry point
- * Return: 0 always
- */
+* main - generates random passwords for 101-crackme
+* Return: Always 0
+*/
 
-int main(void)
+int main (void)
 {
+	int sum = 0;
+	char c;
+
 	srand(time(NULL));
-	printf("%d\n", rand());
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+
+	putchar(2772 - sum);
 	return (0);
 }
