f56abd8d8bf791a91a52a7473a6859304a233b06
FrankDonny <arkohfrank1@gmail.com>
2022-04-26 16:56:57 +0000

    memcpy function

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,9 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *p = src;
-	char *i = dest;
 	unsigned int a;
 
 	for (a = 0; a < n; a++)
-		p[a] = i[a];
+		src[a] = dest[a];
 	return (dest);
 }
