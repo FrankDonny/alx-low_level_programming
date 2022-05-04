f1737c9e77f4ce71ebccc23ed25c01adcc6f8d24
FrankDonny <arkohfrank1@gmail.com>
2022-05-04 20:33:10 +0000

    add positive numbs

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-
+/**
+ * main - Print result of adding given arguments
+ * @argc: Number of arguments
+ * @argv: Arguments received
+ *
+ * Return: 0 on success, 1 if theres a nondigit arg
+ */
 int main(int argc, char *argv[])
 {
+	int sum;
+	int count;
+	int i;
 
-	int i = 0, sum = 0;
-
-	if (argv[i] >= '48' || argv[i] <= '48')
+	count = 1;
+	sum = 0;
+	if (argc == 1)
 	{
 		printf("0\n");
+		return (0);
 	}
-	
-	else if (i < argc && !(argv[i] > '48' || argv[i] < '48'))
+	while (count < argc)
 	{
-		printf("Error\n");
-		return (1);
-		i++;
-	}
-	else
-	{
-		sum += atoi(argv[i]);
+		for (i = 0; argv[count][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[count][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[count]);
+		count++;
 	}
 	printf("%d\n", sum);
 	return (0);
