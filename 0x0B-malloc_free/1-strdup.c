84e5e0effdd8f4c3e0a97a74e114dff4acdff93f
FrankDonny <arkohfrank1@gmail.com>
2022-05-05 14:33:04 +0000

    copy strings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * _strdup - duplicates string
+ * @str: parameter passed to strdup
+ * Return: NULL if error, dest if success
+ */
+
+char *_strdup(char *str)
+{
+	char *dest;
+	unsigned int n = 0;
+
+	dest = malloc(sizeof(char) * n);
+	strcpy(dest, str);
+	return (dest);
+
+	if (str == NULL)
+		return (NULL);
+}
