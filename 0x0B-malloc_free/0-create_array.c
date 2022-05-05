789bb789839e407de4903df20e484fb73c450c82
FrankDonny <arkohfrank1@gmail.com>
2022-05-05 20:53:15 +0000

    _

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,10 +15,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	ptr = malloc(sizeof(char) * size);
+	if (size == 0 || ptr == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
 
-	if (size == 0 || ptr == NULL)
-		return (NULL);
 	return (ptr);
 }
