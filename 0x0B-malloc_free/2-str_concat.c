3fb5a7c602596640f46ada96d220b8a62cd1d40c
FrankDonny <arkohfrank1@gmail.com>
2022-05-05 20:31:00 +0000

    _

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,48 +1,44 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * str_concat - duplicates string
- * @s1: parameter passed to str_concat
- * @s2: parameter passed to str_concat
- * Return: NULL if error, newAlloc if success
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
  */
-
-/**
-* _strlen - returns the length of a string
-* @s: string
-* Return: String length
-*/
-
-int _strlen(char *s)
-{
-	int count = 0;
-
-	while (*s == '\0')
-	{
-		s++;
-		count++;
-		break;
-	}
-	return (count);
-}
-
 char *str_concat(char *s1, char *s2)
 {
-	char *newAlloc;
-	unsigned int n, m;
-	n = m = 0;
-	n = _strlen(s1);
-	m = _strlen(s2);
+	char *strout;
+	unsigned int i, j, k, limit;
 
-	newAlloc = malloc(sizeof(char) * (n + m));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	strcpy (newAlloc, (strcat(s1, s2)));
-	/*strcat(newAlloc, curCat);*/
-	return (newAlloc);
+	for (i = 0; s1[i] != '\0'; i++)
+		;
 
-	if (newAlloc == NULL)
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
+	{
+		free(strout);
 		return (NULL);
+	}
+
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
+
+	return (strout);
 }
