cc2cc35a70ef0a146ef49e138619c8bc83384acf
FrankDonny <arkohfrank1@gmail.com>
2022-05-09 17:01:52 +0000

    _

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,10 +12,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	ptr = malloc(nmemb * size);
 
-	if (nmemb == 0 || size == 0 || ptr == NULL)
+	if ptr == NULL
 		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
 
 	return (ptr);
 }
