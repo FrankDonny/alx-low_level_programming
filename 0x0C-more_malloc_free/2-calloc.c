08aacc5d8c429b4b2e44b01b6c26a5459d690613
FrankDonny <arkohfrank1@gmail.com>
2022-05-09 16:50:59 +0000

    _

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 
-
+/**
+ * _calloc - allocate memory
+ * @nmemb: parameter to _calloc
+ * @size: parameter to _calloc
+ * Return: NULL if error, else ptr
+ */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
