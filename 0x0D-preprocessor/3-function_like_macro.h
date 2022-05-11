390a3e949cdda3f7e34bfb4138066a6abbeed957
FrankDonny <arkohfrank1@gmail.com>
2022-05-11 12:50:35 +0000

    function-like abs macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,3 @@
-#include <stdlib.h>
-
-/**
- * _abs - computes the absolute value
- * @i: the number to compute
- * Return: absolute value
- */
-#ifndef ABS 
+#ifndef ABS
 #define ABS(x) abs(x)
 #endif
