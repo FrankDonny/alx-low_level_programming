984371dace1fcdf01b99f015e6fa9556ca47aa81
FrankDonny <arkohfrank1@gmail.com>
2022-05-11 13:59:17 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new data type
+ * @name: member of the structure
+ * @age: member of the structure
+ * @owner: member of the structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
