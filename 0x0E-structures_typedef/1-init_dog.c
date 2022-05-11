e8752021a882b5becb07a824b0bf063a426dc62d
FrankDonny <arkohfrank1@gmail.com>
2022-05-11 14:59:00 +0000

    initialize dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog structure
+ * @d: structure variable
+ * @name: parameter for name of dog
+ * @age: parameter for age of dog
+ * @owner: parameter for owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
