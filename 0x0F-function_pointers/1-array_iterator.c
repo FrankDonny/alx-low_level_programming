83d74ee9a09364bf9477b77d1b3fbdfc50c2d370
FrankDonny <arkohfrank1@gmail.com>
2022-05-13 15:42:14 +0000

    array interator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,12 +1,22 @@
 #include <stddef.h>
 #include "function_pointers.h"
 
+/**
+ * array_iterator - iterator function
+ * @array: parameter containing the array
+ * @size: parameter of size of array
+ * @action: callback function
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 	
-	for (i = 0; i < size; i++)
+	if (action != NULL)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
