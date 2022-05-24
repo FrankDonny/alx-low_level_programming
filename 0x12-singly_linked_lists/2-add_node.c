00696974f6cb3315cd6c3c65b2b20a39e775f742
FrankDonny <arkohfrank1@gmail.com>
2022-05-24 21:22:02 +0000

    add a new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,17 @@
 #include "lists.h"
 
+/**
+ * add_node - this fucntion adds a new node
+ * @head: this is a double pointer that points to list_t
+ * @str: the string to be copied
+ * Return: NULL if there is no new string, return pointer head otherwise
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	unsigned int len = 0;
-	list_t *s = malloc(sizeof (list_t));
-	
+	list_t *s = malloc(sizeof(list_t));
+
 	while (str[len])
 		len++;
 
