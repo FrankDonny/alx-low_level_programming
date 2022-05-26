6def6650136884baf5c929bf57756c23beb83741
FrankDonny <arkohfrank1@gmail.com>
2022-05-26 11:59:19 +0000

    free allocated memory for listint_t setting head node to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * free_listint2 - function to free listint_t memory allocation
+ * @head: pointer to listint_t
+ */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
