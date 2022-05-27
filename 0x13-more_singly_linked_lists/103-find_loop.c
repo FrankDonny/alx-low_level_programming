d259a2941454f55bc0a745bf5183a271a93f3447
FrankDonny <arkohfrank1@gmail.com>
2022-05-27 18:51:55 +0000

    find loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,24 +1,35 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - find the loop the in an linked list
+ * find_listint_loop - this function find the loop the in an linked list
  * @head: this point to the linked list
  * Return: just return
  */
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *single_pace, *double_pace, *temp;
+	listint_t *single_pace, *double_pace;
 
-	temp = head;
-	single_pace = single_pace->next;
-	double_pace = double_pace->next-next;
+	single_pace = head;
+	double_pace = head;
 
-	while (single_pace && double_pace && double_pace-next)
+	if (!head)
+		return (NULL);
+
+	while (single_pace && double_pace && double_pace->next)
 	{
-		if (single_pace != double_pace)
-			return (NULL);
-		temp = temp->next;
+		single_pace = single_pace->next;
+		double_pace = double_pace->next->next;
+		if (single_pace == double_pace)
+		{
+			single_pace = head;
+			while (double_pace != single_pace)
+			{
+				single_pace = single_pace->next;
+				double_pace = double_pace->next;
+			}
+			return (double_pace);
+		}
 	}
-	return (temp->next);
+	return (NULL);
 }
