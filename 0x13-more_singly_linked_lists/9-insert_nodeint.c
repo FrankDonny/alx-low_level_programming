1a983728d2833312040f2e3f62bbb40555161ab1
FrankDonny <arkohfrank1@gmail.com>
2022-05-27 00:59:25 +0000

    reverses linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	temp = *head;
-	
 
 	if (idx < i || temp == NULL || *head == NULL || newNode == NULL)
 		return (NULL);
