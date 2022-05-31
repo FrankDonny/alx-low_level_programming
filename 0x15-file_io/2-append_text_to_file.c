ab941d22445876728ea8f43c7f9f9e4416001df7
FrankDonny <arkohfrank1@gmail.com>
2022-05-31 00:47:01 +0000

    function to append contents to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,8 +11,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	char *buf_size = malloc(sizeof(text_content));
-	int fp;
-	int wr;
+	int fp, wr, a;
 
 	fp = open(filename, O_WRONLY | O_APPEND, 0600);
 	if (fp == -1)
@@ -21,9 +20,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	wr = write(fp, text_content, *buf_size);
-	if (wr == -1)
+	if (text_content != NULL)
+	{
+		for (a = 0; text_content[a]; a++)
+			;
+		wr = write(fp, text_content, *buf_size);
+		if (wr == -1)
 		return (-1);
-
+	}
 	return (1);
 }
