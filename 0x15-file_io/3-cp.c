43137ed55370047128a8843ce060c65db0239a3a
FrankDonny <arkohfrank1@gmail.com>
2022-05-31 12:22:22 +0000

    program to copy file content from one file to the other

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * close_file_error - detects if a file can't be closed
+ * @file: file to check it's return value on failure
+ */
+
+void close_file_error(int file)
+{
+	if (file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		exit(100);
+	}
+}
+
+
+/**
  * main - program to copy file's content to another
  * @argc: counts the number of arguments added
  * @argv: contains the command line arguments
@@ -10,6 +25,7 @@
 int main(int argc, char *argv[])
 {
 	int rd, wr, file_from, file_to;
+	mode_t permissions;
 	char *buf_size;
 
 	if (argc != 3)
@@ -17,12 +33,11 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	/*read first file*/
 	file_from = open(argv[1], O_RDONLY);
 
 	buf_size = malloc(sizeof(char) * 1024);
-	
+
 	rd = read(file_from, buf_size, 1024);
 	if (!file_from || rd == -1)
 	{
@@ -31,8 +46,10 @@ int main(int argc, char *argv[])
 	}
 
 	/*write to second file*/
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	
+	permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, permissions);
+
 	wr = write(file_to, buf_size, rd);
 	if (file_to == -1 || wr == -1)
 	{
@@ -41,16 +58,8 @@ int main(int argc, char *argv[])
 	}
 
 	close(file_from);
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", file_from);
-		exit(100);
-	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_to);
-		exit(100);
-	}
+	close_file_error(file_from);
+	close_file_error(file_to);
 
 	return (wr);
 
