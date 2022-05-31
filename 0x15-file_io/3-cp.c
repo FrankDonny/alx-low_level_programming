f65ecec7d90cb8f2aeb61c490159d2d94033e038
FrankDonny <arkohfrank1@gmail.com>
2022-05-31 14:55:16 +0000

    program to copy file content from one file to the other

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,8 +26,7 @@ int main(int argc, char *argv[])
 {
 	int rd, wr, file_from, file_to;
 	mode_t permissions;
-	/*char *buf_size;*/
-	char buf_size[1024];
+	char *buf_size;
 
 	if (argc != 3)
 	{
@@ -37,10 +36,10 @@ int main(int argc, char *argv[])
 	/*read first file*/
 	file_from = open(argv[1], O_RDONLY);
 
-	/*buf_size = malloc(sizeof(char) * 1024);*/
+	buf_size = malloc(sizeof(char) * 1024);
 
 	rd = read(file_from, buf_size, 1024);
-	if (rd == -1)
+	if (!file_from || rd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
@@ -52,7 +51,7 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, permissions);
 
 	wr = write(file_to, buf_size, rd);
-	if (wr == -1)
+	if (file_to == -1 || wr == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
