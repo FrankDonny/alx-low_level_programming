9aa1f13af2c54c3be738c9cf06940aad2b84eb1d
FrankDonny <arkohfrank1@gmail.com>
2022-06-01 09:55:27 +0000

    print binary representation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,8 +4,8 @@ void print_binary(unsigned long int n)
 {
 	unsigned int i;
 
-	for (i = n ; (i = 1 << 1) && i > 0; i *= 0.5)
-	{ 
+	for (i = n ; i; i *= 0.5)
+	{
 		(n & i) ? putchar(49) : putchar(48);
 	}
 }
