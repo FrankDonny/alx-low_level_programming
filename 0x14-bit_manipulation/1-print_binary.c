1bbb337510932c5a30ccb4bcc0b1126c24d692fb
FrankDonny <arkohfrank1@gmail.com>
2022-06-01 17:36:59 +0000

    function to find the value of a bit at a given index

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,8 +7,10 @@
 
 void print_binary(unsigned long int n)
 {
-	int i;
+	unsigned int i;
 
-	for (i = n ; i; i *= 0.5)
+	for (i = n; i; i = i * 0.5)
 		(n & i) ? _putchar(49) : _putchar(48);
+	if (n == 0)
+		_putchar(48);
 }
