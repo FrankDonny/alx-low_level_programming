c1f882e3b0c8c8b12b64413d3536c598785d9921
FrankDonny <arkohfrank1@gmail.com>
2022-06-01 20:50:50 +0000

    function to set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,8 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i = 1;
 	if (index > 63)
 		return (-1);
-	*n = *n | (i << index);
+	*n = *n | (1UL << index);
 	return (*n);
 }
