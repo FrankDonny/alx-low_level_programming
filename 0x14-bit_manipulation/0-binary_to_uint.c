b55b02b1e3f48726b813fa2dbf9c77d4b0281118
FrankDonny <arkohfrank1@gmail.com>
2022-06-01 14:06:11 +0000

    converts binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,24 @@
 #include "main.h"
 
+/**
+ * binary_to_uint - converts binary strings to unsigned int
+ * @b: pointer pointing to the string
+ * Return: dec on success, 0 otherwise
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int dec = 0, rem, idx = 1, ch;
-	size_t i;
+	unsigned int dec = 0;
+	int i = 0;
 
-	for(i = 0; i <= strlen(b); i++)
-	{
-		if (*b != 0)
-			return (0);
-		if (*b != 1)
-			return (0);
-		/*if (b == NULL)
-			return (0);*/
-	}
+	if (b == NULL)
+		return (0);
 
-	ch = atoi(b);
-	while (ch != 0)
+	for (i = 0; b[i]; i++)
 	{
-		rem = ch % 10;
-		dec = dec + (rem*idx);
-		ch = ch / 10;
-		idx *= 2;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec = 2 * dec + (b[i] - '0');
 	}
-	return ((unsigned int)dec);
+	return (dec);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * print_binary - This function prints the binary representation of a number
+ * @n: The number that its binary representation to be printed
+ */
+
 void print_binary(unsigned long int n)
 {
 	int i;
