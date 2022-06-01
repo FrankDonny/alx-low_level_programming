2ef1d484c7a77c100bc993d8df005e7c3830741c
FrankDonny <arkohfrank1@gmail.com>
2022-06-01 21:25:01 +0000

    this function clears a bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - This function clears bit
+ * @n: the number the operation is to be performed on
+ * @index: the index to be cleared
+ * Return: n on success, else -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = *n & ~(1 << index);
+	return (*n);
+}
