6624f856acb8d2626659a566ef6bde3596eaf323
FrankDonny <arkohfrank1@gmail.com>
2022-06-02 00:03:14 +0000

    function to return number of bit flips

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * countSB - counts the set bits of a number
+ * @op: the number
+ * Return: number of count
+ */
+
+unsigned int countSB(unsigned int op)
+{
+	unsigned int count = 0;
+
+	while (op)
+	{
+		count += op & 1;
+		op >>= 1;
+	}
+	return (count);
+}
+
+/**
+ * flip_bits - This function detects number of flips to be done for flipping
+ * integers
+ * @n: first number
+ * @m: second number
+ * Return: the number of flips
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int var;
+
+	var = n ^ m;
+	return (countSB(var));
+}
