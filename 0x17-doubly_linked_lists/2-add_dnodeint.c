75d7e1926509fdd16e8efecafba712dbc3684755
FrankDonny <arkohfrank1@gmail.com>
2022-07-07 16:00:44 +0000

    insert node in a doubly linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,32 +1,39 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint - inserts a new node into a linked list
+ * @head: pointer to the linked lists
+ * @n: data of the new node
+ * Return: newNode
+ */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-  dlistint_t *newNode, *temp;
+	dlistint_t *newNode, *temp;
 
-  temp = *head;
+	temp = *head;
 
-  newNode = malloc(sizeof(dlistint_t));
-  if (newNode == NULL)
-  {
-    return NULL;
-  }
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
 
-  newNode->n = n;
-  newNode->prev = NULL;
+	newNode->n = n;
+	newNode->prev = NULL;
 
-  if (temp != NULL)
-  {
-    while (temp->prev != NULL)
-    {
-      temp = temp->prev;
-    }
-  }
-  newNode->next = temp;
-  if (temp != NULL)
-  {
-    temp->prev = newNode;
-  }
-  temp = newNode;
-  return (newNode);
+	if (temp != NULL)
+	{
+		while (temp->prev != NULL)
+		{
+			temp = temp->prev;
+		}
+	}
+	newNode->next = temp;
+	if (temp != NULL)
+	{
+		temp->prev = newNode;
+	}
+	temp = newNode;
+	return (newNode);
 }
