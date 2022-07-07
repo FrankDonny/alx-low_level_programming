705ce4307ac9c7a8888d726e5c4e823a02f61e43
FrankDonny <arkohfrank1@gmail.com>
2022-07-07 21:56:05 +0000

    find the sum of all data of a dlistint linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - this function sums all data of nodes
+ * @head: pointer to the linked list
+ * Return: the sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head->next != NULL)
+	{
+		head = head->next;
+		sum += (*head).n;
+	}
+	return (sum);
+}
