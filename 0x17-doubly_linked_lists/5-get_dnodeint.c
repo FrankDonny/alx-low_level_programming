00058b2210b4a2053b4e5f26d8c7f1a78f3346d1
FrankDonny <arkohfrank1@gmail.com>
2022-07-07 21:38:54 +0000

    add node at an index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,9 +1,15 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - add node at a given index
+ * @head: pointer to the linked list
+ * @index: the position to fix the newnode
+ * Return: return the address of the head
+ */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned count = 0;
+	unsigned int count = 0;
 
 	if (head == NULL)
 		return (NULL);
