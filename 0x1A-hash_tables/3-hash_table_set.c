54cd6644009e05991d9f04f362d2df9f324d7f97
FrankDonny <arkohfrank1@gmail.com>
2022-07-29 12:45:21 +0000

    Hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,11 +20,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
+	{
+		free(newNode);
 		return (0);
+	}
 	if (key == NULL)
 		return (0);
 
 	newNode->value = strdup(value);
+	if (newNode->value == NULL)
+	{
+		free(newNode->key);
+		free(node);
+		return (0);
+	}
 	newNode->next = NULL;
 
 	idx = key_index(((const unsigned char *)key), ht->size);
