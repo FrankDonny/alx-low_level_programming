8eb7ecbdcb6c495cb4605534cce057302c957ac3
FrankDonny <arkohfrank1@gmail.com>
2022-11-01 11:32:37 +0000

    0x1E. C - Search Algorithms

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
 
+/**
+ * jump_search - search algorithm using jump search
+ * @array: array to run the search in
+ * @size: the size of the array
+ * @value: the figure to search for
+ * Return: the index of the value on success, -1 otherwise
+ */
+
 int jump_search(int *array, size_t size, int value)
 {
 	size_t jump = floor(sqrt(size)), ind1, i;
@@ -9,21 +17,24 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%d] = [%d]\n", (int)ind1, (int)ind1);
 		if (array[ind1] == value)
 		{
-			printf("Value found between indexes [%d] and [%d]\n", (int)(ind1 - jump), (int)ind1);
-			/*printf("Value checked array[%d] = [%d]\n", (int)ind1, (int)ind1);*/
-			return ind1;
+			printf("Value found between indexes [%d] and [%d]\n",
+					(int)(ind1 - jump), (int)ind1);
+			return (ind1);
 		}
 		if (array[ind1] > value)
 		{
 			for (i = ind1; array[ind1] > value; i--)
 			{
-				printf("Value checked array[%d] = [%d]\n", (int)i, (int)i);
+				printf("Value checked array[%d] = [%d]\n",
+						(int)i, (int)i);
 				ind1--;
 			}
-			printf("Value found between indexes [%d] and [%d]\n", (int)ind1 - 1, (int)(ind1 + jump - 1));
-			return ind1;
+			printf("Value found between indexes [%d] and [%d]\n",
+					(int)ind1 - 1, (int)(ind1 + jump - 1));
+			return (ind1);
 		}
 	}
-	printf("Value found between indexes [%d] and [%d]\n", (int)(ind1 - jump), (int)ind1);
-	return -1;
+	printf("Value found between indexes [%d] and [%d]\n",
+			(int)(ind1 - jump), (int)ind1);
+	return (-1);
 }
